05031420dc493cc7d26a178eaed45f4eaf34197a
ReanGD <reangd@yandex.ru>
2019-07-03 23:17:20 +0300

    smooth fly controller

--- src/middleware/camera/fly_controller.cpp
+++ src/middleware/camera/fly_controller.cpp
@@ -14,8 +14,12 @@ void FlyCameraController::SetHotkey(Action action, Key key) noexcept {
     }
 }
 
-void FlyCameraController::SetMovementSpeed(float value) noexcept {
-    m_movementSpeed	= value;
+void FlyCameraController::SetMementSmoothFactor(float value) noexcept {
+    m_movementSmoothFactor = value;
+}
+
+void FlyCameraController::SetMovementNormalSpeed(float value) noexcept {
+    m_movementNormalSpeed = value;
 }
 
 void FlyCameraController::SetMouseSmooth(float value) noexcept {
@@ -28,71 +32,69 @@ void FlyCameraController::SetMouseSensitivity(float value) noexcept {
 
 void FlyCameraController::AttachCamera(std::shared_ptr<Camera> camera) {
     camera->SetAspectRatio(m_aspectRatio);
+    camera->SetViewParams(camera->GetPosition(), m_direction);
     m_cameras.push_back(camera);
 }
 
 void FlyCameraController::Update(WindowInput& wio, float deltaTime) {
-    constexpr const float factor = 0.3f;
-    constexpr const float mouseSensitivityFactor = 1.0f / 2000.0f;
-    constexpr const float pitchMax = glm::half_pi<float>() - 0.2f;
-    constexpr const float pitchMin = -(glm::half_pi<float>() - 0.2f);
+    constexpr const auto mouseSensitivityFactor = 1.0f / 2000.0f;
+    constexpr const auto pitchMax = glm::half_pi<float>() - 0.2f;
+    constexpr const auto pitchMin = -(glm::half_pi<float>() - 0.2f);
+    constexpr const auto matOne = glm::mat4(1.0);
+    constexpr const auto yawAxis = glm::vec3(0.0f, 1.0f, 0.0f);
+    constexpr const auto pitchAxis = glm::vec3(0.0f, 0.0f, 1.0f);
+    constexpr const auto vecEye = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
 
-    float dt = deltaTime * (1.0f - factor);
-
-    m_posOffsetPrevious *= factor;
-
+    glm::vec3 positionOffset = glm::vec3(0);
     if (m_enableInput) {
+        // xxxSmoothFactor - for 60 FPS, recalc for our FPS:
+        float smoothFPSCorrection = deltaTime * 60;
+
         bool isMove = false;
-        glm::vec3 posOffset(0);
+        glm::vec3 moveDirection(0);
         if (wio.IsKeyStickyDown(m_hostkey[static_cast<size_t>(Action::Forward)])) {
-            posOffset.x += 1.0f;
+            moveDirection.x += 1.0f;
             isMove = true;
         } else if (wio.IsKeyStickyDown(m_hostkey[static_cast<size_t>(Action::Backward)])) {
-            posOffset.x -= 1.0f;
+            moveDirection.x -= 1.0f;
             isMove = true;
         }
         if (wio.IsKeyStickyDown(m_hostkey[static_cast<size_t>(Action::Right)])) {
-            posOffset.y += 1.0f;
+            moveDirection.y += 1.0f;
             isMove = true;
         } else if (wio.IsKeyStickyDown(m_hostkey[static_cast<size_t>(Action::Left)])) {
-            posOffset.y -= 1.0f;
+            moveDirection.y -= 1.0f;
             isMove = true;
         }
         if (isMove) {
-            m_posOffsetPrevious += (glm::normalize(posOffset) * (m_movementSpeed * dt));
+            auto currentSpeed = glm::normalize(moveDirection) * m_movementNormalSpeed;
+            m_movementSpeed = glm::lerp(currentSpeed, m_movementSpeed, glm::min(m_movementSmoothFactor * smoothFPSCorrection, 0.9f));
+            positionOffset = (m_movementSpeed * deltaTime);
         }
 
-        // mouseSmoothFactor - for 60 FPS, recalc for our FPS:
-        float mouseSmoothFactorFPS = glm::min(m_mouseSmoothFactor * deltaTime * 60, 0.9f);
-
+        bool isRotate = false;
         glm::vec2 dtRotation;
         wio.GetCursorOffet(dtRotation.x, dtRotation.y);
-        glm::vec2 currentRotationSpeed = dtRotation / deltaTime;
-        m_rotationSpeed = glm::lerp(currentRotationSpeed, m_rotationSpeed, mouseSmoothFactorFPS);
-        m_rotation += (m_rotationSpeed * m_mouseSensitivity * mouseSensitivityFactor * deltaTime);
+        if (glm::abs(dtRotation.x) > 0.1f || glm::abs(dtRotation.y) > 0.1f) {
+            isRotate = true;
+            auto currentRotationSpeed = dtRotation / deltaTime;
+            m_rotationSpeed = glm::lerp(currentRotationSpeed, m_rotationSpeed, glm::min(m_mouseSmoothFactor * smoothFPSCorrection, 0.9f));
+            m_rotation += (m_rotationSpeed * m_mouseSensitivity * mouseSensitivityFactor * deltaTime);
 
-        if (m_rotation.x >= glm::two_pi<float>()) {
-            m_rotation.x -= glm::two_pi<float>();
-        } else if (m_rotation.x <= -glm::two_pi<float>()) {
-            m_rotation.x += glm::two_pi<float>();
+            m_rotation.x = std::fmod(m_rotation.x, glm::two_pi<float>());
+            m_rotation.y = glm::clamp(m_rotation.y, pitchMin, pitchMax);
+            m_direction = glm::rotate(matOne, m_rotation.x, yawAxis) * glm::rotate(matOne, m_rotation.y, pitchAxis) * vecEye;
         }
 
-        m_rotation.y = glm::min(glm::max(m_rotation.y, pitchMin), pitchMax);
-    }
-
-    const auto matOne = glm::mat4(1.0);
-    const auto yawAxis = glm::vec3(0.0f, 1.0f, 0.0f);
-    const auto pitchAxis = glm::vec3(0.0f, 0.0f, 1.0f);
-    const auto vecEye = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-
-    glm::vec3 direction = glm::rotate(matOne, m_rotation.x, yawAxis) * glm::rotate(matOne, m_rotation.y, pitchAxis) * vecEye;
-
-    for(const auto& camera: m_cameras) {
-        auto position =
-            camera->GetPosition() +
-            camera->GetDirection() * m_posOffsetPrevious.x +
-            camera->GetLeftVector() * m_posOffsetPrevious.y;
-        camera->SetViewParams(position, direction);
+        if (isMove || isRotate) {
+            for(const auto& camera: m_cameras) {
+                auto position =
+                    camera->GetPosition() +
+                    camera->GetDirection() * positionOffset.x +
+                    camera->GetLeftVector() * positionOffset.y;
+                camera->SetViewParams(position, m_direction);
+            }
+        }
     }
 
     uint32_t width, height;
--- src/middleware/camera/fly_controller.h
+++ src/middleware/camera/fly_controller.h
@@ -22,7 +22,8 @@ public:
 public:
     void EnableInput(bool value) noexcept;
     void SetHotkey(Action action, Key key) noexcept;
-    void SetMovementSpeed(float value) noexcept;
+    void SetMementSmoothFactor(float value) noexcept;
+    void SetMovementNormalSpeed(float value) noexcept;
     void SetMouseSmooth(float value) noexcept;
     void SetMouseSensitivity(float value) noexcept;
     void AttachCamera(std::shared_ptr<Camera> camera);
@@ -30,18 +31,18 @@ public:
     void Update(WindowInput& wio, float deltaTime);
 private:
     bool m_enableInput = true;
-    float m_movementSpeed = 2.0f;
+    float m_movementSmoothFactor = 0.5f;
+    float m_movementNormalSpeed = 5.0f;
     float m_mouseSmoothFactor = 0.5f;
     float m_mouseSensitivity = 10.0f;
     float m_aspectRatio = 4.0f / 3.0f;
 
-    // Offset in the previous frame
-    glm::vec3 m_posOffsetPrevious = glm::vec3(0);
+    glm::vec3 m_movementSpeed = glm::vec3(0);
 
     // Current camera angle in the radians (x: yaw - left/right,  y: pitch - up/down)
     glm::vec2 m_rotation = glm::vec2(0);
-    // Speed in the previous frame
     glm::vec2 m_rotationSpeed = glm::vec2(0);
+    glm::vec3 m_direction = glm::vec3(1.0f, 0.0f, 0.0f);
 
     Key m_hostkey[static_cast<size_t>(FlyCameraController::Action::Last)] = {
         /*Forward*/Key::W, /*Backward*/Key::S, /*Right*/Key::D, /*Left*/Key::A};
